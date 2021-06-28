58aabc597b687f9a62bf74cf79ed384f208f2766
Daniel Morales <mralejo99@gmail.com>
2021-06-28 08:12:03 -0500

    new dependencies

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,33 @@
 const { resolve } = require("path");
 
-module.exports = {
-  type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "test",
-  password: "test",
-  database: "test",
-  synchronize: true,
-  logging: false,
-  entities: [resolve(__dirname, "src/models/*{.ts,.js}")],
-  migrations: [resolve(__dirname, "src/migrations/*{.ts,.js}")],
-  subscribers: [resolve(__dirname, "src/migrations/*{.ts,.js}")],
-};
+module.exports = [
+  {
+    name: "relational",
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "test",
+    password: "test",
+    database: "test",
+    synchronize: true,
+    logging: false,
+    entities: [resolve(__dirname, "src/models/*{.ts,.js}")],
+    migrations: [resolve(__dirname, "src/migrations/*{.ts,.js}")],
+    subscribers: [resolve(__dirname, "src/migrations/*{.ts,.js}")],
+  },
+  {
+    name: "collections",
+    type: process.env.DATABASE_COLLECTIONS_TYPE,
+    url: process.env.DATABASE_COLLECTIONS_URL,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    logging: true,
+    synchronize: true,
+    extra: {
+      authSource: "admin",
+    },
+    entities: [resolve(__dirname, process.env.TYPEORM_ENTITIES)],
+  },
+];
